3b4f9f9746ebf117c15a03a1940be770c73a86d8
Git-DonHub <donjeffreyb@gmail.com>
2024-09-16 11:00:53 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-
-
+num1 = int(input("Enter first number: "))
+action = input('Choose a mathematical operation:\n+\n-,\n*\n/')
+num2 = int(input("Enter second number: "))
+if action == '+':
+    print(num1,'+',num2,'=',num1+num2)
+elif action == '-':
+    print(num1,'-',num2,'=',num1-num2)
+elif action == '*':
+    print(num1,'*',num2,'=',num1*num2)
+elif action == '/':
+    print(num1,'/',num2,'=',num1/num2)
+else:
+    print("Invalid input")
