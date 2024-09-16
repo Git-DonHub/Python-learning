458abab08cb83a531931f61c4f1af66cc9a708b7
Git-DonHub <donjeffreyb@gmail.com>
2024-09-16 10:59:03 -0400

    This is a replica of a basic calculator
    Tried doing it from memory hope its correct

--- main.py
+++ main.py
@@ -1,13 +1,2 @@
-i = input("Enter a number: ")
-i = int(i)
-for number in range(1, i):
-    if number % 3 == 0 and number % 5 == 0:
-        print("FizzBuzz")
-    elif number % 3 == 0:
-        print("Buzz")
-    elif number % 5 == 0:
-        print("Fizz")
-    else:
-        print(number)
 
 
